fa60dac033e6fb34e4fb7f2c0a02613412c4482e
Aishwaryabh18 <hungundaishwarya@gmail.com>
2026-02-08 16:41:19 +0530

    feat: add Redux Toolkit counter example

---
+++ redux-demo/redux_fundamentals.md
@@ -0,0 +1,5 @@
+## When should you use Redux instead of useState?
+
+Redux should be used when state needs to be shared across multiple components or managed globally.
+For small, local component state, useState is simpler and more appropriate.
+Redux becomes useful as applications scale and state logic becomes complex.
---
+++ redux-demo/src/components/Counter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../redux/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
