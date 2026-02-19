eccf883a12b5320c1af2d1256e492098536ee5b5
Mamon81 <jianna727@gmail.com>
2026-02-19 10:25:34 +0800

    feat: create App component with Tailwind styling

---
+++ Milestones/milestone_5/milestone-5-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-950 text-white">
+      <div className="space-y-4 text-center">
+        <h1 className="text-5xl font-black tracking-tighter text-violet-400">
+          Hello World!
+        </h1>
+        <p className="text-zinc-400">Milestone 5 Setting Up A React Project</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
