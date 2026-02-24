fdd929e7ee1ff1d4c219928a475d148243e08462
Mamon81 <jianna727@gmail.com>
2026-02-24 10:29:05 +0800

    feat: Implement Redux for Counter.js file
    - Create Redux store configuration
    - Add counter slice with increment/decrement reducers
    - Connect Counter component to Redux
    - Wrap app with Redux Provider

--- Milestones/milestone_7/milestone-7-react-project/src/pages/Counter.js
+++ Milestones/milestone_7/milestone-7-react-project/src/pages/Counter.js
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from '../slices/counterSlice';
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
 
-  const incrementCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+  const dispatch = useDispatch();
 
   return (
     <div className="flex flex-col items-center gap-2">
@@ -15,7 +14,7 @@ function Counter() {
 
       <button
         type="button"
-        onClick={incrementCount}
+        onClick={() => dispatch(increment())}
         className="mt-2 rounded-lg border-2 border-zinc-600 bg-neutral-200 px-8 py-4 text-2xl font-medium text-orange-300 transition-all hover:bg-orange-900 hover:text-neutral-200 active:scale-95"
       >
         Increment Count
