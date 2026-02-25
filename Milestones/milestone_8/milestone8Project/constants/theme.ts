ce462d1e5c6b7996ee33f1df00a31e23949fa2d7
Mamon81 <jianna727@gmail.com>
2026-02-25 10:46:06 +0800

    feat: refactor ItemList.js component from Milestone 7 to use React Native components - Port item list from web to using React Native components - Replace explore tab with item list screen - Apply orange theme across app with contrast-adjusted text - Simplify home/modal screens to minimal content

--- Milestones/milestone_8/milestone8Project/constants/theme.ts
+++ Milestones/milestone_8/milestone8Project/constants/theme.ts
@@ -5,21 +5,21 @@
 
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#3B1F00';
+const tintColorDark = '#3B1F00';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#3B1F00',
+    background: '#FDBA74',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#3B1F00',
+    background: '#FDBA74',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
