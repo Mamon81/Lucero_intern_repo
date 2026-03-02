0635692061e618c2b666d5628b5ff3922e9a46e2
Mamon81 <jianna727@gmail.com>
2026-03-02 11:21:27 +0800

    feat: add multiplication operation to calculator
    - Added multiply operation to calculator function

--- Milestones/milestone_3/bisect-test.js
+++ Milestones/milestone_3/bisect-test.js
@@ -1,7 +1,8 @@
 const calculator = (a, b, operation) => {
   if (operation === 'add') return a + b;
   if (operation === 'subtract') return a - b;
+  if (operation === 'multiply') return a * b;
   return 0;
 };
 // eslint-disable-next-line no-console
-console.log('Result:', calculator(10, 5, 'subtract'));
+console.log('Result:', calculator(4, 3, 'multiply'));
